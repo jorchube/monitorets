4e18ee5240c12fc5d5c452210fa18cc46afdbca8
Jordi Chulia <jorchube.dev@gmail.com>
2022-11-21 21:31:34 +0100

    Version 0.5.1: * Restored missing network permission needed for the network monitors

--- src/main.py
+++ src/main.py
@@ -65,7 +65,7 @@ class MonitorApplication(Adw.Application):
                                 application_name='Monitorets',
                                 application_icon='io.github.jorchube.monitorets',
                                 developer_name='Jordi Chulia',
-                                version='0.5.0',
+                                version='0.5.1',
                                 developers=['Jordi Chulia'],
                                 copyright='© 2022 Jordi Chulia')
         about.present()
