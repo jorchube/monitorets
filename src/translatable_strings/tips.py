40c1b9aca23e8833cc12694178721dc19b980b6c
Jordi Chulia <jorchube.dev@gmail.com>
2022-11-30 20:51:15 +0100

    Use span attributes instead of pango convenience markup tags for translatable strings

--- src/translatable_strings/tips.py
+++ src/translatable_strings/tips.py
@@ -2,4 +2,4 @@ from gettext import gettext as _
 
 WINDOW_TITLE = _("Tips")
 ALWAYS_ON_TOP_TITLE = _("Always on Top")
-ALWAYS_ON_TOP_BODY = _("You can make the window stay on top of any other window: Press <b><tt>Alt+Space</tt></b> or right click with your mouse in the window titlebar to bring the window menu, then select <b><i>Always on Top</i></b>.")
+ALWAYS_ON_TOP_BODY = _("You can make the window stay on top of any other window: Press <span font_weight='bold'>Alt+Space</span> or right click with your mouse in the window titlebar to bring the window menu, then select <span font_weight='bold' font_style='italic'>Always on Top</span>.")
