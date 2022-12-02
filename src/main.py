d470909ee388f76b32ad657ec8cb08537b8a9265
Jordi Chulia <jorchube.dev@gmail.com>
2022-12-02 20:22:16 +0100

    Bump to version 0.6.0

--- src/main.py
+++ src/main.py
@@ -65,7 +65,7 @@ class MonitorApplication(Adw.Application):
                                 application_name='Monitorets',
                                 application_icon='io.github.jorchube.monitorets',
                                 developer_name='Jordi Chulia',
-                                version='0.5.1',
+                                version='0.6.0',
                                 developers=['Jordi Chulia'],
                                 copyright='© 2022 Jordi Chulia')
         about.present()
