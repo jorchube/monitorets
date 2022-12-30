b61d6921d878976102c3156a1aa2d72418b42d65
Jordi Chulia <jorchube.dev@gmail.com>
2022-12-30 20:39:33 +0100

    Use a symbolic icon in the temerature sensor monitors label

--- src/ui/monitor_widgets/temperature_sensor_monitor_widget.py
+++ src/ui/monitor_widgets/temperature_sensor_monitor_widget.py
@@ -7,7 +7,7 @@ from ...translatable_strings import monitor_title
 class TemperatureSensorMonitorWidget(MonitorWidget):
     def __init__(self, temperature_sensor_descriptor, *args, **kwargs):
         name = f"{temperature_sensor_descriptor.hardware_name}-{temperature_sensor_descriptor.hardware_sensor_name}"
-        self._title = f"{monitor_title.TEMPERATURE} ({name})"
+        self._title = f"🌡{name}"
         self._color = colors.BROWN
         self._monitor = TemperatureMonitor(temperature_sensor_descriptor)
 
