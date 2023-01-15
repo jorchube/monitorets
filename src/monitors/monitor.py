8c55567886d44267b99615bf37b49dd1a535fd0c
Jordi Chulia <jorchube.dev@gmail.com>
2023-01-15 12:50:05 +0100

    Use Sample class instead of raw values in monitor class

--- src/monitors/monitor.py
+++ src/monitors/monitor.py
@@ -5,7 +5,7 @@ class Monitor:
         self._sampler = sampler
         self._sampler.install_new_sample_callback(self._new_sample)
         self._new_values_callback = None
-        self._values = []
+        self._graph_values = []
 
     def start(self):
         self._sampler.start()
@@ -16,8 +16,9 @@ class Monitor:
     def install_new_values_callback(self, cb):
         self._new_values_callback = cb
 
-    def _new_sample(self, value):
-        self._values.insert(0, value)
+    def _new_sample(self, sample):
+        self._last_readable_value = sample.label_value
+        self._graph_values.insert(0, sample.to_plot)
         if self._new_values_callback:
             self._report_values()
 
@@ -25,10 +26,10 @@ class Monitor:
             self._free_old_values()
 
     def _report_values(self):
-        self._new_values_callback(self._values)
+        self._new_values_callback(self._graph_values, self._last_readable_value)
 
     def _has_reached_max_values_stored(self):
-        return len(self._values) > self._MAX_NUMBER_OF_VALUES_STORED
+        return len(self._graph_values) > self._MAX_NUMBER_OF_VALUES_STORED
 
     def _free_old_values(self):
-        self._values = self._values[:self._MAX_NUMBER_OF_VALUES_STORED]
+        self._graph_values = self._graph_values[:self._MAX_NUMBER_OF_VALUES_STORED]
