62818884ddc2698497c77610cf1de25ef113e553
Jordi Chulia <jorchube.dev@gmail.com>
2023-01-16 21:51:20 +0100

    Simplify network monitors hierarchy

--- src/ui/monitor_widgets/downlink_monitor_widget.py
+++ src/ui/monitor_widgets/downlink_monitor_widget.py
@@ -1,11 +1,12 @@
-from .relative_monitor_widget import RelativeMonitorWidget
+from .monitor_widget import MonitorWidget
 from ...monitors.downlink_monitor import DownlinkMonitor
+from ..relative_graph_area import RelativeGraphArea
 from .. import colors
 from ...translatable_strings import monitor_title
 from ...monitor_type import MonitorType
 
 
-class DownlinkMonitorWidget(RelativeMonitorWidget):
+class DownlinkMonitorWidget(MonitorWidget):
     def __init__(self, *args, **kwargs):
         self._type = MonitorType.Downlink
         self._title = monitor_title.DOWNLINK
@@ -13,3 +14,6 @@ class DownlinkMonitorWidget(RelativeMonitorWidget):
         self._monitor = DownlinkMonitor()
 
         super().__init__(self._monitor, self._type, self._title, self._color, *args, **kwargs)
+
+    def _graph_area_instance(self, color, redraw_freq_seconds, draw_smooth_graph):
+        return RelativeGraphArea(color, redraw_freq_seconds, draw_smooth_graph)
--- src/ui/monitor_widgets/uplink_monitor_widget.py
+++ src/ui/monitor_widgets/uplink_monitor_widget.py
@@ -1,11 +1,12 @@
-from .relative_monitor_widget import RelativeMonitorWidget
+from .monitor_widget import MonitorWidget
 from ...monitors.uplink_monitor import UplinkMonitor
+from ..relative_graph_area import RelativeGraphArea
 from .. import colors
 from ...translatable_strings import monitor_title
 from ...monitor_type import MonitorType
 
 
-class UplinkMonitorWidget(RelativeMonitorWidget):
+class UplinkMonitorWidget(MonitorWidget):
     def __init__(self, *args, **kwargs):
         self._type = MonitorType.Uplink
         self._title = monitor_title.UPLINK
@@ -13,3 +14,6 @@ class UplinkMonitorWidget(RelativeMonitorWidget):
         self._monitor = UplinkMonitor()
 
         super().__init__(self._monitor, self._type, self._title, self._color, *args, **kwargs)
+
+    def _graph_area_instance(self, color, redraw_freq_seconds, draw_smooth_graph):
+        return RelativeGraphArea(color, redraw_freq_seconds, draw_smooth_graph)
