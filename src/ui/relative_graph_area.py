c061acd8ab362f29285fd34db14791ea7ff4fa8a
Jordi Chulia <jorchube.dev@gmail.com>
2023-01-17 21:28:37 +0100

    Support for unified scale in up and downlink monitors

--- src/ui/relative_graph_area.py
+++ src/ui/relative_graph_area.py
@@ -2,8 +2,11 @@ from .graph_area import GraphArea
 
 
 class RelativeGraphArea(GraphArea):
-    def __init__(self, color, redraw_frequency_seconds, draw_smooth_graph):
-        self._minimum_relative_value = 1000
+    def __init__(self, color, redraw_frequency_seconds, draw_smooth_graph, new_reference_value_callback):
+        self._minimum_reference_value = 1000
+        self._reference_value = self._minimum_reference_value
+        self._own_reference_value = self._minimum_reference_value
+        self._new_reference_value_callback = new_reference_value_callback
         super().__init__(color, redraw_frequency_seconds, draw_smooth_graph)
 
     def set_new_values(self, values):
@@ -11,17 +14,28 @@ class RelativeGraphArea(GraphArea):
         return super().set_new_values(normalized_values)
 
     def _normalize_values(self, values):
-        max_value = max(max(values), self._minimum_relative_value)
+        self._refresh_reference_value(values)
+
         normalized_values = []
 
         for value in values:
-            normalized_value = self._calculate_normalized_value(value, max_value)
+            normalized_value = self._calculate_normalized_value(value)
             normalized_values.append(normalized_value)
 
         return normalized_values
 
-    def _calculate_normalized_value(self, value, max_value):
-        if max_value == 0:
+    def _calculate_normalized_value(self, value):
+        if self._reference_value == 0:
             return 0
 
-        return int(value * 100 / max_value)
+        return int(value * 100 / self._reference_value)
+
+    def _refresh_reference_value(self, values):
+        previous_own_reference_value = self._own_reference_value
+        self._own_reference_value = max(max(values), self._minimum_reference_value)
+
+        if previous_own_reference_value != self._own_reference_value:
+            self._new_reference_value_callback(self._own_reference_value)
+
+    def set_reference_value(self, value):
+        self._reference_value = value
