8a0ccc473628ab864dd094934fb0d99d46339939
Jordi Chulia <jorchube.dev@gmail.com>
2023-01-17 21:14:27 +0100

    Add network monitor scale related events

--- src/events.py
+++ src/events.py
@@ -5,6 +5,10 @@ MONITOR_DISABLED = "monitor disabled"
 
 MONITOR_RENAMED = "monitor renamed"
 
+UPLINK_NETWORK_MONITOR_NEW_REFERENCE_VALUE_PROPOSAL = "uplink network monitor new reference value proposal"
+DOWNLINK_NETWORK_MONITOR_NEW_REFERENCE_VALUE_PROPOSAL = "downlink network monitor new reference value proposal"
+NETWORK_MONITOR_NEW_REFERENCE_VALUE = "uplink network monitor new reference value"
+
 ABOUT_DIALOG_TRIGGERED = "about dialog triggered"
 TIPS_DIALOG_TRIGGERED = "tips dialog triggered"
 
