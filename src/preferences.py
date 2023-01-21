63496e48b86219e4a9e0b4a356818b4ae11ce0b3
Jordi Chulia <jorchube.dev@gmail.com>
2023-01-21 19:43:24 +0100

    Handle WindowGeometry in preferences

--- src/preferences.py
+++ src/preferences.py
@@ -7,6 +7,7 @@ from . import events
 from .preference_keys import PreferenceKeys
 from .theme import Theme
 from .layout import Layout
+from .window_geometry import WindowGeometry
 
 
 class Preferences:
@@ -18,6 +19,7 @@ class Preferences:
         PreferenceKeys.LAYOUT: Layout.VERTICAL,
         PreferenceKeys.SMOOTH_GRAPH: True,
         PreferenceKeys.SHOW_CURRENT_VALUE: False,
+        PreferenceKeys.WINDOW_GEOMETRY: WindowGeometry(width=180, height=40).as_dict(),
         PreferenceKeys.CPU_MONITOR_ENABLED: True,
         PreferenceKeys.CPU_PER_CORE_MONITOR_ENABLED: False,
         PreferenceKeys.GPU_MONITOR_ENABLED: False,
@@ -34,17 +36,40 @@ class Preferences:
         "custom_name": {},
     }
 
-    _preferences = {}
+    _preferences = dict()
+    _custom_key_handler = dict()
+
+    @classmethod
+    def initialize(self):
+        self._custom_key_handler[PreferenceKeys.WINDOW_GEOMETRY] = {
+                "set": self._set_window_geometry,
+                "get": self._get_window_geometry,
+            }
 
     @classmethod
     def get(self, preference_path):
-        return self._preferences[preference_path]
+        if preference_path in self._custom_key_handler:
+            return self._custom_key_handler[preference_path]["get"]()
+
+        return self._default_get_handler(preference_path)
 
     @classmethod
     def set(self, preference_path, value):
+        if preference_path in self._custom_key_handler:
+            self._custom_key_handler[preference_path]["set"](value)
+            return
+
+        self._default_set_handler(preference_path, value)
+
+    @classmethod
+    def _default_set_handler(self, preference_path, value):
         self._preferences[preference_path] = value
         self._persist_preferences()
         EventBroker.notify(events.PREFERENCES_CHANGED, preference_path, value)
+
+    @classmethod
+    def _default_get_handler(self, preference_path):
+        return self._preferences[preference_path]
 
     @classmethod
     def get_custom_name(self, monitor_type):
@@ -110,3 +135,12 @@ class Preferences:
     def _migrate_deprecated_adaptive_layout_value(self):
         if Preferences.get(PreferenceKeys.LAYOUT) == "adaptive":
             Preferences.set(PreferenceKeys.LAYOUT, Layout.VERTICAL)
+
+    @classmethod
+    def _set_window_geometry(self, window_geometry):
+        self._default_set_handler(PreferenceKeys.WINDOW_GEOMETRY, window_geometry.as_dict())
+
+    @classmethod
+    def _get_window_geometry(self):
+        window_geometry_dict = self._default_get_handler(PreferenceKeys.WINDOW_GEOMETRY)
+        return WindowGeometry.from_dict(window_geometry_dict)
