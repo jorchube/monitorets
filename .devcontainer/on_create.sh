e042f24f7a9acfe91bbe59cc032bfc807481f817
Jordi Chulia <jorchube.dev@gmail.com>
2023-02-11 10:45:21 +0000

    trivial change to trigger CI actions

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -6,4 +6,4 @@ dnf -y install vim make python3-devel python3-gobject libadwaita-devel gtk4-deve
 pip install pipenv
 
 echo ". /usr/share/git-core/contrib/completion/git-prompt.sh" >> ~/.bashrc
-echo -e "export PS1='\e[01;34m\]\w\e[01;33m\]\$(__git_ps1)\n\e[00;01m\]$\e[00m\] '" >> ~/.bashrc
+echo -e "export PS1='\e[01;34m\]\w\e[01;35m\]\$(__git_ps1)\n\e[00;01m\]$\e[00m\] '" >> ~/.bashrc
