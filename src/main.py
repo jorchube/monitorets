e4fd2a6ad74fed4a0ccdfb1bfbedfe1d9a0b19f1
Jordi Chulia <jorchube@users.noreply.github.com>
2023-03-18 20:36:15 +0100

    Prepare for 0 10 0 (#82)
    <!-- Hello! Please try to answer the following questions so that a
    reviewer can better understand the changes proposed in this PR.
    Thank you :) -->
    
    ## What?
    <!-- Please describe the change introduced in this PR -->
    
    ## Why?
    <!-- Please explain the motivation behind this change -->
    
    ## How could a reviewer see this new change in action?
    <!-- Try to give a hint on how a reviewer could easily take a look at
    the change introduced in this PR -->

--- src/main.py
+++ src/main.py
@@ -76,7 +76,7 @@ class MonitorApplication(Adw.Application):
             application_name="Monitorets",
             application_icon="io.github.jorchube.monitorets",
             developer_name="Jordi Chulia",
-            version="0.9.2",
+            version="0.10.0",
             developers=["Jordi Chulia"],
             copyright="© 2022 Jordi Chulia",
             translator_credits=translators_credits.strip(),
